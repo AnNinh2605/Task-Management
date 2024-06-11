d1b5e44fc3db30cf270e5337c7542d0c5f80ff62
AnNinh2605 <anninh1511@gmail.com>
2024-06-11 22:02:18 +0700

    <feat> Responsive for Login & Register

--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -1,39 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const [showPassword, setShowPassword] = useState(false);
 
     return (
-        <div className='d-flex justify-content-center vh-100 align-items-center'>
-            <div className='col-3 border rounded-4 p-3 bg-secondary text-white d-flex flex-column'>
-                <h5>Login</h5>
-                <form className='d-flex flex-column gap-3'>
-                    <div className='form-group'>
-                        <label htmlFor="email">Email</label>
+        <div className='d-flex flex-column justify-content-center align-items-center gap-2 vh-100 bg text-white'>
+            <h2>Task Management</h2>
+            <div className='col-10 col-sm-6 col-md-3 rounded-4 p-3 bg-main d-flex flex-column gap-2'>
+                <h4>Login</h4>
+
+                <form>
+                    <div>
                         <input
                             type="email"
                             id='email'
                             className='form-control'
                             placeholder='Email'
+                            autoComplete='on'
                         />
                     </div>
-                    <div className='position-relative'>
-                        <label htmlFor="password">Password</label>
+                    <div className='position-relative mt-2'>
                         <input
-                            type="password"
+                            type={showPassword ? "text" : "password"}
                             id='password'
                             placeholder='Password'
                             className='form-control'
+                            autoComplete='current-password'  
                         />
-                       <i className="position-absolute end-0 top-50 text-black pe-2 pt-1 fa-solid fa-eye-slash"></i> 
-                        {/* <i className="position-absolute end-0 top-50 text-black pe-2 pt-1 fa-solid fa-eye"></i> */}
+                        {
+                            !showPassword ?
+                                <i className="position-absolute end-0 translate-middle top-50 text-black fa-solid fa-eye-slash" onClick={() => setShowPassword(!showPassword)}></i> :
+                                <i className="position-absolute end-0 top-50 translate-middle text-black fa-solid fa-eye" onClick={() => setShowPassword(!showPassword)}></i>
+                        }
                     </div>
                 </form>
-                <button type='submit' className='btn btn-primary mt-4 rounded-3 px-3'>
+
+                <button type='submit' className='btn btn-primary rounded-3 mt-2'>
                     Login
                 </button>
-                <a className='text-center text-decoration-none'>Forgotten password?</a>
-                <hr />
-                <button className='btn btn-success w-75 mx-auto'>Create new account</button>
+
+                <Link to='#' className='text-center text-decoration-none'>Forgot password?</Link>
+                <hr className='my-1' />
+
+                <button className='btn btn-success rounded-3 mx-auto'
+                    onClick={() => navigate('/register')}>
+                    Create new account
+                </button>
             </div>
         </div>
     );
--- Client/src/Components/Register.jsx
+++ Client/src/Components/Register.jsx
@@ -1,46 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const defaultPasswordStatus = {
+        password: false,
+        confirmPassword: false
+    }
 
+    const [passwordStatus, setPasswordStatus] = useState(defaultPasswordStatus);
+
+    const togglePasswordVisibility = (name) => {
+        setPasswordStatus(prevStatus => ({
+            ...prevStatus,
+            [name]: !prevStatus[name]
+        }));
+    };
+
+    const passwordInput = (id, placeholder, field) => {
+        return (
+            <div className='position-relative mt-2'>
+                <input
+                    type={field ? "text" : "password"}
+                    id={id}
+                    placeholder={placeholder}
+                    className='form-control'
+                    autoComplete='current-password'
+                />
+                {
+                    !field ?
+                        <i className="position-absolute end-0 translate-middle top-50 text-black fa-solid fa-eye-slash" onClick={() => togglePasswordVisibility(id)}></i> :
+                        <i className="position-absolute end-0 top-50 translate-middle text-black fa-solid fa-eye" onClick={() => togglePasswordVisibility(id)}></i>
+                }
+            </div>
+        )
+    }
+    
     return (
-        <div className='d-flex justify-content-center vh-100 align-items-center'>
-            <div className='col-3 border rounded-4 p-3 bg-secondary text-white d-flex flex-column gap-3'>
-                <h5>Login</h5>
-                <form className='d-flex flex-column gap-3'>
-                    <div className='form-group'>
-                        <label htmlFor="email">Email</label>
+        <div className='d-flex flex-column justify-content-center align-items-center gap-2 vh-100 bg text-white'>
+            <h2>Task Management</h2>
+            <div className='col-10 col-sm-6 col-md-3 rounded-4 p-3 bg-main d-flex flex-column gap-2'>
+                <h4>Register</h4>
+
+                <form>
+                    <div>
                         <input
                             type="email"
                             id='email'
                             className='form-control'
                             placeholder='Email'
+                            autoComplete='on'
                         />
                     </div>
-                    <div className='position-relative'>
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id='password'
-                            placeholder='Password'
-                            className='form-control'
-                        />
-                       <i className="position-absolute end-0 top-50 text-black pe-2 pt-1 fa-solid fa-eye-slash"></i> 
-                        {/* <i className="position-absolute end-0 top-50 text-black pe-2 pt-1 fa-solid fa-eye"></i> */}
-                    </div>
-                    <div className='position-relative'>
-                        <label htmlFor="confirmPassword">Confirm password</label>
-                        <input
-                            type="password"
-                            id='confirmPassword'
-                            placeholder='Confirm password'
-                            className='form-control'
-                        />
-                       <i className="position-absolute end-0 top-50 text-black pe-2 pt-1 fa-solid fa-eye-slash"></i> 
-                        {/* <i className="position-absolute end-0 top-50 text-black pe-2 pt-1 fa-solid fa-eye"></i> */}
-                    </div>
+                    {
+                        passwordInput("password", "Password", passwordStatus.password)
+                    }
+                    {
+                        passwordInput("confirmPassword", "Confirm password", passwordStatus.confirmPassword)
+                    }
                 </form>
-                <button className='btn btn-success'>Register</button>
-                <a className='text-center text-decoration-none'>Already have an account?</a>
+                <button className='btn btn-success mt-2'>Register</button>
+
+                <hr className='my-1' />
+
+                <Link to='/login' className='text-center text-decoration-none'>Already have an account?</Link>
             </div>
         </div>
     );
